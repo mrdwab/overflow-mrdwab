67a409448e1d973ca6cd4c025fcdb7c79e3a9c64
Ananda Mahto <mrdwab@gmail.com>
2015-12-30 22:59:55 +0530

    oops

--- R/readClip.R
+++ R/readClip.R
@@ -7,18 +7,25 @@
 #' @return character string containing text on the clipboard.
 #' 
 
-writeClip <- function(object){
+readClip <- function() {
   OS <- Sys.info()["sysname"]
   
-  if(!(OS %in% c("Darwin", "Windows", "Linux"))) stop("Copying to clipboard not yet supported on your OS")
-  
-  switch(OS,
-         "Darwin"={con <- pipe("pbcopy", "w")
-         writeLines(object, con=con)
-         close(con)},
-         "Windows"=writeClipboard(object, format = 1),
-         "Linux"={if(Sys.which("xclip") == "") warning("Clipboard on Linux requires 'xclip'. Try using:\nsudo apt-get install xclip")
-           con <- pipe("xclip -selection clipboard -i", open="w")
-           writeLines(object, con=con)
-           close(con)})
+  cliptext <- switch(
+    OS,
+    Darwin = {
+      con <- pipe("pbpaste")
+      text <- readLines(con)
+      close(con)
+      text
+      },
+    Windows = readClipboard(),
+    Linux = {
+      if (Sys.which("xclip") == "") warning("Clipboard on Linux requires 'xclip'. Try using:\nsudo apt-get install xclip")
+      con <- pipe("xclip -o -selection clipboard")
+      text <- readLines(con = con)
+      close(con)
+      text
+      },
+    stop("Reading from clipboard not yet supported on your OS"))
+  cliptext
 }
